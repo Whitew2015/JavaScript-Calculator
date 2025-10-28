205b59ebd6a5bc1a8fa8332fda0abc06357c0aa1
Winston White <whitew2015@fau.edu>
2025-10-28 15:12:49 -0400

    Implement calculator render and UI

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
 import './App.css';
-import { useEffect, useState, useReducer } from 'react';
+import {  useReducer } from 'react';
+import DigitButton from './DigitButton';
+import OperationButton from './OperationButton';
 
+//objects for certain actions when using the calculator
+export const actions = {
+  add_digit: 'add-digit',
+  choose_operation: 'choose-operation',
+  clear: 'clear',
+  evaluate: 'evaluate'
+}
 
-
-
-
-
-
+//function that controls state changes in response to actions sent by your buttons
+const reducer = (state, { type, payload }) => {
+  //decides how to update the state
+  switch(type) {
+    case actions.add_digit:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`
+      }
+  }
+}
 
 function App() {
+  //initialize reducer
+  const [{ currentOperand, previousOperand, operation }, dispatch] = 
+  useReducer(reducer, {})
+
+  //Test dispatch
+  dispatch( { type: actions.add_digit, payload: { digit: 1}})
+
   return (
     <div className="calculator-grid">
       <div className="output" id="display">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two" id="clear">AC</button>
-      <button id="divide">/</button>
-      <button id="multiply">*</button>
-      <button id="seven">7</button>
-      <button id="eight">8</button>
-      <button id="nine">9</button>
-      <button id="subtract">-</button>
-      <button id="four">4</button>
-      <button id="five">5</button>
-      <button id="six">6</button>
-      <button id="add">+</button>
-      <button id="one">1</button>
-      <button id="two">2</button>
-      <button id="three">3</button>
-      <button className="span-two" id="equals">=</button>
-      <button className="span-two" id="zero">0</button>
-      <button id="decimal">.</button>
+      <OperationButton operation="/" dispatch={dispatch}/>
+      <OperationButton operation="*" dispatch={dispatch}/>
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch}/>
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch}/>
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="=" dispatch={dispatch}/>
+      <DigitButton digit="0" dispatch={dispatch} />
+      <OperationButton operation="." dispatch={dispatch}/>
     </div>
   );
 }
